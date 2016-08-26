44bb53511a85c6d65e3f2f2101a002a1b398acfc
Sergio Isidoro <sergio@holvi.com>
2016-08-26 17:04:55 +0300

    Use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,25 +1,41 @@
-import os
-from distutils.core import setup
+#!/usr/bin/python
+from pip.req import parse_requirements
+import subprocess
+from datetime import datetime
 
-def read(fname):
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), fname)) as f:
-        return f.read()
+from setuptools import setup, find_packages
+
+# Parses pip requirements and transform this
+# into an array to be used at setup time
+install_reqs = [str(req.req) for req in
+                parse_requirements("./requirements.txt", session={})]
+
+def get_version():
+    # Return the time of the last GIT commit as a version
+    # string, for example 20160902T1325. There is a minor chance
+    # of commit time collision, but should be minor enough to
+    # not worry about. Unfortunately we don't get same version
+    # numbers in staging and production.
+    call = subprocess.Popen(
+        ['git', 'log', '-1', '--format=%cd', '--date=iso'],
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, err = call.communicate()
+    if err:
+        raise Exception("Could not get last commit's date from GIT, "
+                        "error was %s" % err)
+    return datetime.strptime(
+        output, '%Y-%m-%d %H:%M:%S +0000\n'
+    ).strftime('%Y%m%dT%H%M')
+
+print(get_version())
 
 setup(
-        name='haystack-cloudsearch',
-        version='0.2',
-        description='An Amazon Cloudsearch backend for Haystack',
-        long_description=read('README.rst'),
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: Apache Software License',
-            'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
-            'Framework :: Django',
-        ],
-        author='Brandon Adams',
-        author_email='emidln@gmail.com',
-        url='https://github.com/pbs/haystack-cloudsearch',
-        license='Apache License (2.0)',
-        py_modules=['haystack_cloudsearch'],
+    name='haystack-cloudsearch',
+    version=get_version(),
+    author='Sergio Isidoro',
+    author_email='sergio@holvi.com',
+    packages=find_packages(exclude=('tests')),
+    install_requires=install_reqs,
+    zip_safe=False,
+    include_package_data=True
 )
